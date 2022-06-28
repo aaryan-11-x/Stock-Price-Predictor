f8b642ffac1eb26132b8ca72f71c999a30b595f4
Aaryan Golatkar <102362952+aaryan-11-x@users.noreply.github.com>
2022-06-28 20:23:27 +0530

    Add files via upload

---
+++ Stock Price Predictor_Github.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+from matplotlib import style
+
+plt.style.use('bmh')
+style.use('ggplot')
