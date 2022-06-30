01a540d61e59b32afcc46847c24bbb87a30c1860
Aaryan Golatkar <102362952+aaryan-11-x@users.noreply.github.com>
2022-06-30 09:03:29 +0530

    Update Stock Price Predictor_Github.py

--- Stock Price Predictor_Github.py
+++ Stock Price Predictor_Github.py
@@ -8,3 +8,24 @@ from matplotlib import style
 
 plt.style.use('bmh')
 style.use('ggplot')
+
+# Enter Your File Name Here
+df = pd.read_csv('WIPRO.NS.csv')
+
+x = df[['High', 'Open', 'Low', 'Volume']].values
+y = df['Close'].values
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
+
+rg = LinearRegression()
+rg.fit(x_train, y_train)
+y_pred = rg.predict(x_test)
+result = pd.DataFrame({'Actual': y_test.flatten(), 'Predicted': y_pred.flatten()})
+
+# Get The Close Price
+df = df[['Close']]
+
+# Create A Variable To Predict 'X' Days Into The Future
+f_days = 25
+df['Prediction'] = df[['Close']].shift(-f_days)
+print(df.tail(4))  # Used To Get The Last 'n' Rows
